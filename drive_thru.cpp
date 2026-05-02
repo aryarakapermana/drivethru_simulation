7ff80a69b317e43c21253e7b8506bc0ee0ade29a
Arya Vorii <aryarakapermana1@gmail.com>
2026-05-02 16:57:34 +0700

    Update drive_thru.cpp

--- drive_thru.cpp
+++ drive_thru.cpp
@@ -1,91 +1,75 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
+
 #define MAX 4
+string plat[MAX];
 
-//define si variable dan array
-struct AntrianDriveThru {
-    string plat[MAX];
-    int front,rear;
+int front = -1;
+int rear = -1;
 
-//kosongin front dan rear
-    AntrianDriveThru() {
-        front=-1;
-        rear =-1;
-    }
-// cek kosong
-    bool isEmpty() {
-        return(front==-1 ||front>rear);
-    }
-// cek penuh
-    bool isFull() {
-        return(rear==MAX-1);
-    }
+bool isEmpty(){
+	return (front == -1 || front>rear);
+}
 
-//tambah mobil
-    void enqueue(string platNomor) {
-        if (isFull()) {
-            cout<<"Jalur Drive-Thru Penuh! Mobil "<<platNomor<<" tidak bisa masuk.\n";
-        } else {
-            if(isEmpty())front = 0;
-            rear++;
-            plat[rear]=platNomor;
-            cout<<"Mobil "<<platNomor<<" masuk antrian.\n";
-        }
-    }
+bool isFull(){
+	return (rear == MAX -1);
+	
+}
+
+//tambahin mobil ke antrian
+void enqueue(string platNomor){
+	if (isFull()){
+		cout<<"Jalur Drive-Thru Penuh! Mobil "<<platNomor<<" tidak bisa masuk.\n";
+	}
+	else {
+		if(isEmpty()) front = 0;
+		rear++;
+		plat[rear]=platNomor;
+		cout<<"Mobil dengan plat "<<platNomor<<" berhasil masuk antrian.";
+	}
+	cout<<endl;
+}
 
 //layanin mobil
-    void dequeue() {
-        if (isEmpty()) {
-            cout<<"Jalur kosong, nggak ada mobil dilayani.\n";
-        } else {
-            cout<<"Melayani mobil plat "<<plat[front]<<endl;
-            front++;
-            if (front>rear){
-                front=-1;
-                rear=-1;
-            }
-        }
-    }
+void dequeue(){
+	if (isEmpty()){
+		cout<<"Jalur kosong! Tidak ada mobil yang bisa di layani.\n";
+	}
+	else{
+		cout<<"Melayani pesanan mobil plat "<<plat[front]<<endl;
+		front++;
+	}
+	cout<<endl;
+}
 
-//lihat antrian
-    void display() {
-        if (isEmpty()) {
-            cout<<"Antrian kosong.\n";
-        } else {
-            cout<<"\n=== Daftar Mobil ===\n";
-            for (int i =front;i<=rear;i++) {
-                cout<<(i-front+1)<< ". "<< plat[i]<<endl;
-            }
-            cout<<"====================\n";
-        }
-    }
-};
+void display(){
+	if (isEmpty()){
+		cout<<"Jalur antrian kosong!.\n";
+	}
+	else {
+		cout<<"\n=== Daftar Antrian Mobil Saat ini ===\n";
+		for (int i=front;i<=rear;i++){
+			cout<<i+1<<". "<<plat[i]<<endl;
+		}
+		cout<<"====================\n";
+	}
+	cout<<endl;
+}
 
-//program utama
-int main() {
-    cout<<"--- SIMULASI DRIVE THRU ---\n";
-
-    AntrianDriveThru antri;
-//tambahin 4 mobil
-    antri.enqueue("B 1234 ABC");
-    antri.enqueue("D 5678 EFG");
-    antri.enqueue("L 9012 HIJ");
-    antri.enqueue("N 3456 KLM");
-//kasih lihat 4 mobil di atas
-    antri.display();
-//ngetes queue penuh
-    antri.enqueue("A 7890 XYZ");
-//layanin 2 mobil
-    antri.dequeue();
-    antri.dequeue();
-//cek sisanya
-    antri.display();
-//layanin semua
-    antri.dequeue();
-    antri.dequeue();
-//ngetes queue kosong
-    antri.dequeue();
-
-    cout << "\n--- SELESAI ---\n";
-    return 0;
+int main(){
+	cout<<"\n--- SIMULASI DRIVE-THRUE DIMULAI ---\n";
+	enqueue("B 1234 ABC");
+	enqueue("D 5678 EFG");
+	enqueue("L 9012 HIJ");
+	enqueue("N 3456 KLM");
+	display();
+	enqueue("A 7890 XYZ");
+	dequeue();
+	dequeue();
+	display();
+	dequeue();
+	dequeue();
+	
+	cout<<"\n--- SIMULASI SELESAI ---\n";
+	
 }
